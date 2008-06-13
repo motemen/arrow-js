583333b24a159758470eee8c17c52a86d3c4efc5
motemen <motemen@gmail.com>
2008-06-13 20:30:05 +0900

    Arrow.prototype.and

--- Arrow.js
+++ Arrow.js
@@ -8,6 +8,20 @@ function Arrow(f) {
     this.func = f;
 }
 
+Arrow.Constant = function(value) {
+    return Arrow(function() {
+        return value;
+    });
+};
+
+Arrow.prototype.call = function() {
+    return this.func.apply(this, arguments);
+};
+
+Arrow.prototype.apply = function(obj, args) {
+    return this.func.apply(obj, args);
+};
+
 Arrow.prototype.next = function(g) {
     var f = this.func;
     return Arrow(function() {
@@ -15,6 +29,12 @@ Arrow.prototype.next = function(g) {
     });
 };
 
-Arrow.prototype.call = function() {
-    return this.func.apply(this, arguments);
+Arrow.prototype.and = function(g) {
+    var f = this.func;
+    return Arrow(function() {
+        return [
+            f.apply(this, arguments),
+            Arrow(g).apply(this, arguments)
+        ];
+    });
 };
