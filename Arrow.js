601fd7d081c9d49a6da0df4df4327410829d6901
motemen <motemen@gmail.com>
2008-06-14 13:24:34 +0900

    * Name arrows  * &&&, *** implemented

--- Arrow.js
+++ Arrow.js
@@ -1,12 +1,20 @@
 function Arrow(f) {
-    if (f instanceof Arrow) {
+    if (f instanceof Arrow)
         return f;
-    }
-    if (!(this instanceof Arrow)) {
+    if (!(this instanceof Arrow))
         return new Arrow(f);
-    }
+
     if (f) {
         this.cpsFunction = function(x, k) { return k(f(x)) };
+        this.name = f.name || this.name;
+    }
+}
+
+Arrow.named = function(name) {
+    return function() {
+        var arrow = Arrow.apply(this, arguments);
+        arrow.name = name;
+        return arrow;
     }
 }
 
@@ -16,23 +24,72 @@ Arrow.fromCPS = function(cpsFunction) {
     return arrow;
 }
 
-Arrow.constant = function(value) {
-    return Arrow(function() {
-        return value;
-    });
+Arrow.fromCPS.named = function(name) {
+    return function() {
+        var arrow = Arrow.fromCPS.apply(this, arguments);
+        arrow.name = name;
+        return arrow;
+    }
+};
+
+Arrow.constant = function(x) {
+    return Arrow.named('const ' + x)(function() { return x });
 }
 
-Arrow.prototype.next = function(g) {
-    var f = this.cpsFunction, g = Arrow(g).cpsFunction;
-    return Arrow.fromCPS(function(x, k) {
-        f(x, function(y) { g(y, k) });
-    });
-}
+Arrow.prototype.name = '(no name)';
 
 Arrow.prototype.call = function(x) {
     var result;
-    this.cpsFunction(x, function(y) { result = y });
+    this.callCPS(x, function(y) { result = y });
     return result;
+}
+
+Arrow.prototype.callCPS = function(x, k) {
+    this.cpsFunction(x, k);
+}
+
+Arrow.prototype.toString = function() {
+    return '[Arrow' + (this.name ? ' ' + this.name : '') + ']';
+}
+
+Arrow.prototype['>>>'] = function(g) {
+    var f = this, g = Arrow(g);
+    return Arrow.fromCPS.named(f.name + ' >>> ' + g.name)(function(x, k) {
+        f.callCPS(x, function(y) { g.callCPS(y, k) });
+    });
+}
+
+Arrow.prototype.next = Arrow.prototype['>>>'];
+
+Arrow.prototype['&&&'] = function(g) {
+    var f = this, g = Arrow(g);
+    return Arrow.fromCPS.named(f.name + ' &&& ' + g.name)(function(x, k) {
+        var results = { };
+        function callCont() {
+            if ('f' in results && 'g' in results) {
+                k([results.f, results.g]);
+            }
+        }
+        f.callCPS(x, function(y) { results.f = y; callCont() });
+        g.callCPS(x, function(y) { results.g = y; callCont() });
+    });
+}
+
+Arrow.prototype['***'] = function(g) {
+    var f = this, g = Arrow(g);
+    return Arrow.fromCPS.named(f.name + ' *** ' + g.name)(function(x, k) {
+        if (typeof x == 'undefined') {
+            x = [];
+        }
+        var results = { };
+        function callCont() {
+            if ('f' in results && 'g' in results) {
+                k([results.f, results.g]);
+            }
+        }
+        f.callCPS(x[0], function(y) { results.f = y; callCont() });
+        g.callCPS(x[1], function(y) { results.g = y; callCont() });
+    });
 }
 
 Arrow.Delay = function(msec) {
